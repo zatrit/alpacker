f5b3a824da3991e1874aa70d217cf0c0eb0f096f
zatrit <zatrit@gmail.com>
2025-04-04 12:21:23 +0300

    added ability to build without filesystem dependency

---
+++ alpacker/src/fs.rs
@@ -0,0 +1,87 @@
+use std::{
+    collections::HashMap,
+    fs::{self, File},
+    io,
+    path::{Path, PathBuf},
+};
+
+use serde::{Deserialize, Serialize};
+use thiserror::Error;
+
+use crate::{JsonIoError, Pack};
+
+/// The name of the manifest file that stores metadata about available asset packs.
+pub const MANIFEST_FILE: &'static str = "manifest.json";
+
+#[derive(Debug, Error)]
+pub enum PackLoadError {
+    #[error("I/O error: {0}")]
+    Io(#[from] io::Error),
+
+    #[error("No pack \"{0}\" found")]
+    NoSuchPack(&'static str),
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+#[repr(transparent)]
+pub struct PackMeta(pub PathBuf);
+
+/// Represents a collection of asset packs.
+///
+/// This struct keeps track of the directory containing asset packs
+/// and a mapping of pack names to their metadata.
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Assets {
+    packs_dir: PathBuf,
+    packs: HashMap<String, PackMeta>,
+}
+
+impl Assets {
+    /// Creates a new [Assets] instance.
+    ///
+    /// # Arguments
+    /// * `packs_dir` - The root directory where asset packs are stored.
+    /// * `packs` - A mapping of pack names to their metadata.
+    pub fn new(packs_dir: impl Into<PathBuf>, packs: HashMap<String, PackMeta>) -> Self {
+        let packs_dir = packs_dir.into();
+        Self { packs_dir, packs }
+    }
+
+    /// Loads asset metadata from a directory containing a manifest file.
+    ///
+    /// # Arguments
+    /// * `path` - The directory containing the `manifest.json` file.
+    ///
+    /// # Returns
+    /// [Ok]
+    /// * `Ok(Assets)` if the manifest is successfully loaded.
+    /// * `Err(JsonIoError)` if an I/O or deserialization error occurs.
+    pub fn load_from_dir(path: impl AsRef<Path>) -> Result<Self, JsonIoError> {
+        let path = path.as_ref().canonicalize()?;
+        let buf = fs::read_to_string(path.join(MANIFEST_FILE))?;
+
+        let mut assets = serde_json::from_str::<Self>(&buf)?;
+        assets.packs_dir = path.join(assets.packs_dir);
+
+        Ok(assets)
+    }
+
+    /// Loads an asset pack by name.
+    ///
+    /// # Arguments
+    /// * `name` - The name of the asset pack to load.
+    ///
+    /// # Returns
+    /// * `Ok(P)` if the pack is successfully loaded.
+    /// * `Err(PackLoadError)` if the pack is missing or fails to load.
+    pub fn load_pack<P: Pack>(&self, name: &'static str) -> Result<P, PackLoadError> {
+        let Some(meta) = self.packs.get(name) else {
+            return Err(PackLoadError::NoSuchPack(name));
+        };
+
+        let path = self.packs_dir.join(&meta.0).canonicalize()?;
+        let file = File::open(path)?;
+
+        P::load(file).map_err(PackLoadError::Io)
+    }
+}
--- alpacker/src/lib.rs
+++ alpacker/src/lib.rs
@@ -1,17 +1,16 @@
 pub mod data;
 pub mod pack;
 
-use serde::{Deserialize, Serialize};
+#[cfg(feature = "fs")]
+mod fs;
+#[cfg(feature = "fs")]
+pub use fs::*;
+
 use std::{
-    collections::HashMap,
-    fs::{self, File},
     hash, io,
     path::{Path, PathBuf},
 };
 use thiserror::Error;
-
-/// The name of the manifest file that stores metadata about available asset packs.
-pub const MANIFEST_FILE: &'static str = "manifest.json";
 
 // Defines the default hasher to use for hash maps.
 // Uses `twox-hash` if the feature is enabled, otherwise falls back to `RandomState`.
@@ -26,15 +25,6 @@ pub type DefaultHasher = hash::RandomState;
 pub type AssetResult<A: Asset> = Result<A, A::Error>;
 
 #[derive(Debug, Error)]
-pub enum PackLoadError {
-    #[error("I/O error: {0}")]
-    Io(#[from] io::Error),
-
-    #[error("No pack \"{0}\" found")]
-    NoSuchPack(&'static str),
-}
-
-#[derive(Debug, Error)]
 pub enum JsonIoError {
     #[error("I/O error: {0}")]
     Io(#[from] io::Error),
@@ -43,75 +33,11 @@ pub enum JsonIoError {
     Json(#[from] serde_json::Error),
 }
 
-#[derive(Debug, Serialize, Deserialize)]
-#[repr(transparent)]
-pub struct PackMeta(pub PathBuf);
-
-/// Represents a collection of asset packs.
-///
-/// This struct keeps track of the directory containing asset packs
-/// and a mapping of pack names to their metadata.
-#[derive(Debug, Serialize, Deserialize)]
-pub struct Assets {
-    packs_dir: PathBuf,
-    packs: HashMap<String, PackMeta>,
-}
-
 /// Represents a raw file inside an asset pack.
 pub struct Raw<R: io::Read + io::Seek> {
     pub size_hint: Option<usize>,
     pub path: PathBuf,
     pub read: R,
-}
-
-impl Assets {
-    /// Creates a new [Assets] instance.
-    ///
-    /// # Arguments
-    /// * `packs_dir` - The root directory where asset packs are stored.
-    /// * `packs` - A mapping of pack names to their metadata.
-    pub fn new(packs_dir: impl Into<PathBuf>, packs: HashMap<String, PackMeta>) -> Self {
-        let packs_dir = packs_dir.into();
-        Self { packs_dir, packs }
-    }
-
-    /// Loads asset metadata from a directory containing a manifest file.
-    ///
-    /// # Arguments
-    /// * `path` - The directory containing the `manifest.json` file.
-    ///
-    /// # Returns
-    /// [Ok]
-    /// * `Ok(Assets)` if the manifest is successfully loaded.
-    /// * `Err(JsonIoError)` if an I/O or deserialization error occurs.
-    pub fn load_from_dir(path: impl AsRef<Path>) -> Result<Self, JsonIoError> {
-        let path = path.as_ref().canonicalize()?;
-        let buf = fs::read_to_string(path.join(MANIFEST_FILE))?;
-
-        let mut assets = serde_json::from_str::<Assets>(&buf)?;
-        assets.packs_dir = path.join(assets.packs_dir);
-
-        Ok(assets)
-    }
-
-    /// Loads an asset pack by name.
-    ///
-    /// # Arguments
-    /// * `name` - The name of the asset pack to load.
-    ///
-    /// # Returns
-    /// * `Ok(P)` if the pack is successfully loaded.
-    /// * `Err(PackLoadError)` if the pack is missing or fails to load.
-    pub fn load_pack<P: Pack>(&self, name: &'static str) -> Result<P, PackLoadError> {
-        let Some(meta) = self.packs.get(name) else {
-            return Err(PackLoadError::NoSuchPack(name));
-        };
-
-        let path = self.packs_dir.join(&meta.0).canonicalize()?;
-        let file = File::open(path)?;
-
-        P::load(file).map_err(PackLoadError::Io)
-    }
 }
 
 /// Trait for types that can be loaded from asset packs
