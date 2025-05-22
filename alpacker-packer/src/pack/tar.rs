ebdc35b0b5426a5c9b1d47f3208aff892e08799a
zatrit <zatrit@gmail.com>
2025-05-22 15:18:09 +0300

    tar: Simplified tar creation code

--- alpacker-packer/src/pack/tar.rs
+++ alpacker-packer/src/pack/tar.rs
@@ -1,11 +1,5 @@
-use std::{
-    borrow::Cow,
-    fs::{File, read_dir},
-    io,
-    path::Path,
-};
-
 use alpacker::pack::TarPack;
+use std::{borrow::Cow, io, path::Path};
 
 use crate::MakePack;
 
@@ -24,28 +18,7 @@ impl MakePack for TarPack {
     fn make(root: impl AsRef<Path>, write: impl io::Write) -> io::Result<()> {
         let root = root.as_ref();
         let mut tar = tar::Builder::new(write);
-
-        for entry in read_dir(root)? {
-            let entry = match entry {
-                Ok(entry) => entry,
-                Err(err) => {
-                    eprintln!("{err}");
-                    continue;
-                }
-            };
-
-            let name = entry.file_name();
-            let path = root.join(&name);
-
-            let file_type = entry.file_type()?;
-            if file_type.is_dir() {
-                tar.append_dir_all(&name, path)?;
-            } else if file_type.is_file() {
-                let mut file = File::open(path)?;
-                tar.append_file(&name, &mut file)?;
-            }
-        }
-
+        tar.append_dir_all("", root)?;
         tar.finish()
     }
 
